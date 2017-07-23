df63ad86fb6f93e52e0e8b6292d14e2ab6baf4ed
Ivaylo Marinov <ivcho_marinov97@abv.bg>
2017-07-23 16:34:27 +0200

    25.Lambda and LINQ - Exercises

---
+++ LambdaAndLinq - Exercises/01.RegisteredUsers/RegisteredUsers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisteredUsers
+{
+    public class Users
+    {
+        public string Name { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Users> usersData = new List<Users>();
+
+            string[] nameAndRegEntry = Console.ReadLine().Split(new string[] { " -> " },StringSplitOptions.RemoveEmptyEntries);
+
+            while (nameAndRegEntry[0] != "end")
+            {
+                string name = nameAndRegEntry[0];
+                DateTime registrationDate = DateTime.ParseExact
+                    (nameAndRegEntry[1], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                Users newUser = new Users
+                {
+                    Name = name,
+                    RegistrationDate = registrationDate
+                };
+                usersData.Add(newUser);
+
+                nameAndRegEntry = Console.ReadLine().Split(new string[] { " -> " },StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            usersData.Reverse();
+            usersData = usersData.OrderBy(r => r.RegistrationDate).Take(5).OrderByDescending(r => r.RegistrationDate).ToList();
+
+            foreach (var user in usersData)
+            {
+                Console.WriteLine(user.Name);
+            }
+        }
+    }
+}
---
+++ LambdaAndLinq - Exercises/04.SoftUniBeerPong/SoftUniBeerPong.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniBeerPong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teamsData = new Dictionary<string, Dictionary<string, double>>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "stop the game")
+            {
+                string[] tokens = inputLine.Split('|');
+                string player = tokens[0];
+                string team = tokens[1];
+                double playerPoints = double.Parse(tokens[2]);
+
+                if (!teamsData.ContainsKey(team))
+                {
+                    teamsData.Add(team, new Dictionary<string, double>());
+                }
+
+                if (teamsData[team].Count < 3)
+                {
+                    if (!teamsData[team].ContainsKey(player))
+                    {
+                        teamsData[team].Add(player, playerPoints);
+                    }
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            int ranking = 1;
+
+            foreach (var team in teamsData.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                if (team.Value.Count == 3)
+                {
+                    Console.WriteLine($"{ranking}. {team.Key}; Players:");
+                    foreach (var player in team.Value.OrderByDescending(x => x.Value))
+                    {
+                        Console.WriteLine($"###{player.Key}: {player.Value}");
+                    }
+                    ranking++;
+                }
+
+            }
+        }
+    }
+}
