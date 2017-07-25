76cb0553b451fd010fb472348847f9a6c009f87e
Ivaylo Marinov <ivcho_marinov97@abv.bg>
2017-07-26 00:44:41 +0200

    22.Nested Dictionaries - Lab

---
+++ NestedDictionaries - Lab/03.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RecordUniqueNames
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            HashSet<string> uniqueNamesCollection = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int each = 0; each < n; each++)
+            {
+                string name = Console.ReadLine();
+
+                uniqueNamesCollection.Add(name);
+            }
+
+            foreach (var name in uniqueNamesCollection)
+            {
+                Console.WriteLine($"{name}");
+            }
+        }
+    }
+}
