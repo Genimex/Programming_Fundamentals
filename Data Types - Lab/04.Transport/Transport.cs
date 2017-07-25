c5b52950d59821c2e7b6d656cef9cfb6e3da5496
Ivaylo Marinov <ivcho_marinov97@abv.bg>
2017-07-25 22:29:09 +0200

    08.Data Types - Lab

---
+++ Data Types - Lab/04.Transport/Transport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberOfPersons = double.Parse(Console.ReadLine());
+            double courses = numberOfPersons / (4 + 8 + 12);
+            Console.WriteLine("{0}",Math.Ceiling(courses));
+        }
+    }
+}
