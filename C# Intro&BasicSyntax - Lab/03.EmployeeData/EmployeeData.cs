63aa21a470d913e3c44ff4fdc71ab77b12e5c618
Ivaylo Marinov <ivcho_marinov97@abv.bg>
2017-07-25 22:32:35 +0200

    03.C# Intro&BasicSyntax - Lab

---
+++ C# Intro&BasicSyntax - Lab/03.EmployeeData/EmployeeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            double monthlySalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeID:D8}");
+            Console.WriteLine($"Salary: {monthlySalary:f2}");
+        }
+    }
+}
